b5e3392a2e267615e93431f0dc4430049a5a20f0
ing-brian-dev <ingbvn0@gmail.com>
2026-07-07 12:56:14 -0600

    Auth view register and login added

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/auth/register', function () {
+    return view('auth.register');
+})-> name('register');
+
+Route::get('/auth/login', function () {
+    return view('auth.login');
+})-> name('login');
