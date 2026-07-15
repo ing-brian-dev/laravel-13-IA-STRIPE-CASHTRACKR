15403f748ddc41191e5e24b93f0f6771dca2bcb0
ing-brian-dev <ingbvn0@gmail.com>
2026-07-15 16:07:04 -0600

    Feat: avoid unverified users get in to dashboard test

--- tests/Feature/LoginUserTest.php
+++ tests/Feature/LoginUserTest.php
@@ -6,40 +6,76 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 uses(RefreshDatabase::class);
 
 test('shows the login screen', function () {
-    $response = $this -> get(route('login'));
+    $response = $this->get(route('login'));
 
-    $response -> assertOk();
+    $response->assertOk();
 });
 
-it('logs in a virified user successfully', function(){
-    User::factory() -> create([
+it('logs in a virified user successfully', function () {
+    User::factory()->create([
         'email' => 'brian@gmail.com',
         'password' => bcrypt('ickkcki3p2.W'),
+        'email_verified_at' => now(),
+    ]);
+
+    $response = $this->post(route('login.store'), [
+        'email' => 'brian@gmail.com',
+        'password' => 'ickkcki3p2.W',
+    ]);
+
+    $response->assertRedirect(route('dashboard'));
+    $this->assertAuthenticated();
+});
+
+it('does not log in with invalid credentials', function () {
+    User::factory()->create([
+        'email' => 'brian@gmail.com',
+        'password' => bcrypt('ickkcki3p2.W'),
+    ]);
+
+    $response = $this->from(route('login'))->post(route('login.store'), [
+        'email' => 'brian@gmail.com',
+        'password' => 'incorrect-password',
+    ]);
+
+    $response->assertRedirect(route('login'));
+    $response->assertSessionHas('error', 'credenciales incorrectas');
+
+    $this->assertGuest();
+});
+
+it('prevents unverified user from accessing dashboard', function () {
+    User::factory()->unverified()->create([
+        'email' => 'brian@gmail.com',
+        'password' => bcrypt('ickkcki3p2.W'),
+    ]);
+
+    $response = $this->post(route('login.store'), [
+        'email' => 'brian@gmail.com',
+        'password' => 'ickkcki3p2.W',
+    ]);
+
+    $response->assertRedirect(route('dashboard'));
+    $this->assertAuthenticated();
+
+    $dashboardResponse = $this->get(route('dashboard'));
+    $dashboardResponse->assertRedirect(route('verification.notice'));
+});
+
+it('does not allow access to dashboard if email is not verified', function () {
+    $user = User::factory() -> create([
+        'email_verified_at' => null
+    ]);
+
+    $response = $this -> actingAs($user) -> get(route('dashboard'));
+    $response -> assertRedirect(route('verification.notice'));
+});
+
+it('allow access to dashboard if email is verified', function () {
+    $user = User::factory() -> create([
         'email_verified_at' => now()
     ]);
 
-    $response = $this -> post(route('login.store'),[
-        'email' => 'brian@gmail.com',
-        'password' => 'ickkcki3p2.W'
-    ]);
-
-    $response -> assertRedirect(route('dashboard'));
-    $this -> assertAuthenticated();
-});
-
-it('does not log in with invalid credentials', function(){
-    User::factory() -> create([
-        'email' => 'brian@gmail.com',
-        'password' => bcrypt('ickkcki3p2.W'),
-    ]);
-
-    $response = $this ->from(route('login')) -> post(route('login.store'),[
-        'email' => 'brian@gmail.com',
-        'password' => 'incorrect-password'
-    ]);
-
-    $response -> assertRedirect(route('login'));
-    $response -> assertSessionHas('error','credenciales incorrectas');
-
-    $this -> assertGuest();
+    $response = $this -> actingAs($user) -> get(route('dashboard'));
+    $response -> assertOk();
 });
