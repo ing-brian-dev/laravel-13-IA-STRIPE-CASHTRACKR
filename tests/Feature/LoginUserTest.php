a667119e094a802016d604d999ee51ece7301f5a
ing-brian-dev <ingbvn0@gmail.com>
2026-07-15 15:55:28 -0600

    Feat: logs in a virified user successfully

--- tests/Feature/LoginUserTest.php
+++ tests/Feature/LoginUserTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 uses(RefreshDatabase::class);
@@ -8,4 +9,20 @@ test('shows the login screen', function () {
     $response = $this -> get(route('login'));
 
     $response -> assertOk();
+});
+
+it('logs in a virified user successfully', function(){
+    User::factory() -> create([
+        'email' => 'brian@gmail.com',
+        'password' => bcrypt('ickkcki3p2.W'),
+        'email_verified_at' => now()
+    ]);
+
+    $response = $this -> post(route('login.store'),[
+        'email' => 'brian@gmail.com',
+        'password' => 'ickkcki3p2.W'
+    ]);
+
+    $response -> assertRedirect(route('dashboard'));
+    $this -> assertAuthenticated();
 });
