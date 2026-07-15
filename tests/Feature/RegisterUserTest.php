c5fcc207a17cb4a24a8f183463936bc08cacb770
ing-brian-dev <ingbvn0@gmail.com>
2026-07-15 13:48:05 -0600

    Feat: register form test

--- tests/Feature/RegisterUserTest.php
+++ tests/Feature/RegisterUserTest.php
@@ -1,12 +1,51 @@
 <?php
 
-it('shows the registration screen', function (){
-    $response = $this -> get(route('register'));
-    $response -> assertOk();
-    $response -> assertStatus(200);
-    $response -> assertSee('Registrarme');
-    $response -> assertSeeInOrder([
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Event;
+
+uses(RefreshDatabase::class);
+
+it('shows the registration screen', function () {
+    $response = $this->get(route('register'));
+    $response->assertOk();
+    $response->assertStatus(200);
+    $response->assertSee('Registrarme');
+    $response->assertSeeInOrder([
         'Crear cuenta',
-        'Registrarme'
+        'Registrarme',
+    ]);
+});
+
+it('register a new user as unverified and dispatches the registered event', function () {
+
+    Event::fake();
+
+    $response = $this->post(route('register.store'), [
+        'name' => 'Pedriti',
+        'email' => 'pedtiri@gmail.com',
+        'password' => 'ickkcki3p2.W',
+        'password_confirmation' => 'ickkcki3p2.W',
+    ]);
+
+    $response->assertRedirect(route('verification.notice'));
+    $user = User::where('email', 'pedtiri@gmail.com')->first();
+
+    expect($user)->not->toBeNull();
+    expect($user->name)->toBe('Pedriti');
+    expect($user->email)->toBe('pedtiri@gmail.com');
+    expect($user->hasVerifiedEmail())->toBeFalse();
+
+    Event::assertDispatched(Registered::class);
+});
+
+it('should validate required fields wjen the request body is empty', function () {
+    $response = $this->post(route('register.store'), []);
+
+    $response->assertSessionHasErrors([
+        'name' => 'El nombre es requerido',
+        'email' => 'El email es requerido',
+        'password' => 'La contraseña es requerida',
     ]);
 });
